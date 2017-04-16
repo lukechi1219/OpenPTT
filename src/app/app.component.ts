aedd777515e9f52469efa4cb9eecd2a46764a001
Luke Chi <luke.chi@gmail.com>
2017-04-16 15:07:47 +0800

    add http code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Http} from '@angular/http';
+import {Component, PipeTransform, Injectable, Pipe} from '@angular/core';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app works!';
+  data: any;
+  detail: string;
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+
+    const url = '/Ajax.php?ajaxpage=conversation&&num=1239&forumname=Soft_Job';
+//    console.info(url);
+
+    this.http.get(url)
+      .map(x => x.json())
+      .subscribe(
+        (data) => this.data = data,
+        (err) => console.error(err)
+      );
+
+    const url2 = '/ResponesPage.php?u=/bbs/Soft_Job/M.1492279678.A.4EE.html';
+//    console.info(url2);
+
+    this.http.get(url2)
+      .map(x => x.text())
+      .subscribe(
+        (data) => {
+          this.detail = '<div id=\'pttcontent\' >' + data.split('<div id=\'pttcontent\' >')[1];
+        },
+        (err) => console.error(err)
+      );
+  }
+
 }
